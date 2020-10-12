b881828e003aa65d1fdcb1c07b40bd06225be726
Zack Kern <zkkern@gmail.com>
2020-10-12 13:12:57 -0700

    Completed node-notes-cli requested revisions

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,32 +1,21 @@
 /* eslint-disable no-console */
-const myFile = './data.json';
 const dataJ = require('./data.json');
 const fs = require('fs');
 if (process.argv[2] === 'read') {
-  fs.readFile(myFile, 'utf8', (err, data) => {
-    if (err) throw err;
-    const parseStuff = dataJ.notes;
-    const countStuff = Object.keys(parseStuff).length;
-    for (var i = 1; i <= countStuff; i++) {
-      console.log(`${i}: ${parseStuff[i]}`);
-    }
-  });
+  const parseStuff = dataJ.notes;
+  for (const property in parseStuff) {
+    console.log(`${property}: ${parseStuff[property]}`);
+  }
 } else if (process.argv[2] === 'create') {
-  fs.readFile(myFile, 'utf8', (err, data) => {
+  const parseStuff = dataJ.notes;
+  parseStuff[dataJ.nextId] = process.argv[3];
+  var finalWrite = JSON.stringify(dataJ, null, 2);
+  fs.writeFile('data.json', finalWrite, err => {
     if (err) throw err;
-    const parseStuff = dataJ.notes;
-    const countStuff = Object.keys(parseStuff).length;
-    const countPlus = countStuff + 1;
-    parseStuff[countPlus.toString()] = process.argv[3];
-    var finalWrite = JSON.stringify(dataJ, null, 2);
-    fs.writeFile('data.json', finalWrite, err => {
-      if (err) throw err;
-    });
   });
 } else if (process.argv[2] === 'delete') {
   var deleteKey = process.argv[3].toString();
   delete dataJ.notes[deleteKey];
-  var finalWrite = JSON.stringify(dataJ, null, 2);
   fs.writeFile('data.json', finalWrite, err => {
     if (err) throw err;
   });
