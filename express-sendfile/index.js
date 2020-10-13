4733f9b687bd85e7e8fe7d6bd17da6bd4b7afc0f
Zack Kern <zkkern@gmail.com>
2020-10-13 15:31:44 -0700

    Completed express-sendfile revisions || Zack Kern

--- express-sendfile/index.js
+++ express-sendfile/index.js
@@ -6,18 +6,14 @@ app.use((req, res, next) => {
   switch (req.originalUrl) {
     case '/index.html':
       res.sendFile('./index.html', options);
-      res.sendStatus(200);
       break;
     case '/main.js':
       res.sendFile('./main.js', options);
-      res.sendStatus(200);
       break;
     case '/styles.css':
       res.sendFile('./styles.css', options);
-      res.sendStatus(200);
       break;
     default:
-      res.sendStatus(404);
       break;
   }
 });
