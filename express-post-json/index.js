71f78a1127ab618939fe4d10f329c1524bd6e4d7
Zack Kern <zkkern@gmail.com>
2020-10-14 13:29:20 -0700

    Completed express-post-json revisions || Zack Kern

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -7,10 +7,12 @@ app.get('/api/grades', function (req, res) {
   res.json(grades);
 });
 app.post('/api/grades', (req, res) => {
+  res.status(201);
   nextId++;
-  req.body.nextId = nextId;
+  req.body.id = nextId;
   res.json(req.body);
   grades.push(req.body);
+
 });
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
