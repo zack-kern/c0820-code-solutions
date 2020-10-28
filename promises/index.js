90e0af033ed5b380e7d0c33bb64c14a95055b5f1
Zack Kern <zkkern@gmail.com>
2020-10-28 15:25:53 -0700

    Completed promises revision to include error promise handing || Zack Kern

--- promises/index.js
+++ promises/index.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars, no-console */
 const takeAChance = require('./take-a-chance');
 var name = takeAChance('Zack Kern');
-name.then((resolve, reject) => {
+name.then(resolve => {
   if (resolve !== undefined) {
     console.log(resolve);
-  } else {
-    console.log(reject.error);
   }
-});
+})
+  .catch(err => {
+    console.error(err);
+  });
